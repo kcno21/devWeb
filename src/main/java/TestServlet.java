a7db7bb9fd37af872869b56548d20f50e23e767b
kcno21 <kcno21@nate.com>
2012-09-27 10:26:02 +0900

    2

---
+++ src/main/java/TestServlet.java
@@ -0,0 +1,53 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestServlet
+ */
+@WebServlet("/TestServlet")
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TestServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html; charset=euc-kr");
+		
+		PrintWriter out = response.getWriter();
+		System.getProperties();
+		out.print("java.runtime.name : " + System.getProperty("java.runtime.name"));
+		out.print("java.vm.version : " + System.getProperty("java.vm.version"));
+		out.print("java.vm.vendor : " + System.getProperty("java.vm.vendor"));
+		out.print("os.name : " + System.getProperty("os.name"));
+		out.print("os.version : " + System.getProperty("os.version"));
+		out.print("user.dir : " + System.getProperty("user.dir"));
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
